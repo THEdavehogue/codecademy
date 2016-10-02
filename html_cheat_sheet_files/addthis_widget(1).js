dd053843a3445cac01a0aef80375be9e6a3e0cb3
Dave Hogue <thedavehogue@gmail.com>
2016-10-02 14:56:06 -0600

    add html cheat sheet

---
+++ html_cheat_sheet_files/addthis_widget(1).js
@@ -0,0 +1,121 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>K9 Web Protection Alert: Category Blocked</title>
+  <link href="reset.css" rel="stylesheet" type="text/css" />
+  <link href="embedded.css" rel="stylesheet" type="text/css" />
+  <script src="k9.js" type="text/javascript"></script>
+</head>
+<body class="blk3">
+<div id="pageBody">
+  <div id="pageHead">
+    <h1 class="alert">K9 Web Protection Alert</h1>
+  </div>
+  <div id="noMenu">
+  </div>
+  <div id="pageMain">
+    <div id="subMenu">
+      <h2 id="title-block">Filtering Alert</h2>
+      <div id="subMenuContainer">
+      </div>
+    </div>
+    <div id="pageContent">
+      <h3 class="block" id="title-blk3">Category Blocked</h3>
+    <div id="notifier"></div>
+<div class="summary bodyDiv">
+  The site you tried to visit belongs to a category that your computer is set to block.
+</div>
+<div class="details bodyDiv">
+  <span class="url">s7.addthis.com/js/250/addthis_widget.js</span> is blocked because it is currently categorized as: 
+<dl class="category">
+  <dt class="blockCat" id="category-88">
+    <a href="#category-88">Web Advertisements</a>
+  </dt>
+  <dd>
+    Sites that provide online advertisements or banners.  These sites will always be allowed.  Does not include advertising servers which serve adult-oriented advertisements.
+  </dd>
+</dl>
+
+</div>
+<div class="blkSep">
+
+</div>
+
+<div class="button">
+  <a class="button" href="#" id="btn-back">
+    <span class="title">Go Back</span>
+    <span class="description">Return to the previous page.</span>
+  </a>
+</div>
+<div class="blkSep">
+
+</div>
+<div class="buttonset bodyDiv smallButtons">
+  <h4 class="noPreceed">You may also choose from the following administrative options:</h4>
+
+  <div class="blockPageAdminControlsDiv">
+<div class="button">
+  <a class="button admin" href="#" id="btn-allowSite">
+    <span class="title">Allow This Site</span>
+    <span class="description">Allow access to <span class="url">http://s7.addthis.com</span> 
+<select id="btn-allowSite-dd">
+  <option value="5">for 5 minutes</option>
+  <option value="15" selected="selected">for 15 minutes</option>
+  <option value="30">for 30 minutes</option>
+  <option value="60">for 60 minutes</option>
+  <option value="-1">permanently</option>
+</select>
+</span>
+  </a>
+</div>
+
+<div class="button">
+  <a class="button admin" href="#" id="btn-allowCat">
+    <span class="title">Allow This Category</span>
+    <span class="description">Allow access to Web sites rated as Web Advertisements 
+<select id="btn-allowCat-dd">
+  <option value="5">for 5 minutes</option>
+  <option value="15" selected="selected">for 15 minutes</option>
+  <option value="30">for 30 minutes</option>
+  <option value="60">for 60 minutes</option>
+  <option value="-1">permanently</option>
+</select>
+</span>
+  </a>
+</div>
+</div><span class="clear"></span>
+
+</div>
+<div class="clear"></div>
+<div class="buttonset bodyDiv smallButtons">
+  
+  <div class="blockPageAdminOptsDiv">
+<div class="button">
+  <a class="button" href="http://s7.addthis.com/js/250/addthis_widget.js" id="btn-dispute">
+    <span class="title">Request Site Review</span>
+    <span class="description"></span>
+  </a>
+</div>
+
+<div class="button">
+  <a class="button admin" href="#" id="btn-url-categories">
+    <span class="title">Change Your Settings</span>
+    <span class="description"></span>
+  </a>
+</div>
+</div>
+</div>
+<div class="clear"></div>
+<img src="http://127.0.0.1:2372/k9bark" style="display:none;width:1px;height:1px;" />
+<br><div class='supervisorModeInfoDiv'>To enter Supervisor Mode, which allows all Web access, go to the <a class='advPageUrl' href='http://127.0.0.1:2372/other'>Other Settings Page</a></div>    </div>
+  </div>
+  <div id="blueCoat"></div>
+  <div id="copyright">
+    Copyright &copy; 2006-2009 Blue Coat Systems, Inc.  All Rights Reserved.
+  </div>
+</div>
+<div id="floater"></div>
+<div id="selection"></div>
+</body>
+</html>
