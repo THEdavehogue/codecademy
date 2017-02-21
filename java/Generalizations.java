16991486ffd8d77f23b644fb51a1308bfac8d5a6
Dave Hogue <thedavehogue@gmail.com>
2017-02-21 12:38:08 -0700

    finish intro to Java

---
+++ java/Generalizations.java
@@ -0,0 +1,12 @@
+public class Generalizations {
+	public static void main(String[] args) {
+
+
+		boolean isComplete = true; // Comment to explain variable
+		int awesomeLevel = 121;
+		int epicLevel = awesomeLevel * 2;
+		System.out.println(epicLevel);
+
+
+	}
+}
